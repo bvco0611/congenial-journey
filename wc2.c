92634fc91ad2f621010ee56bf52cfc101d8375af
BRUNO VINICIUS CASSOL ORENGO <16104011@l1801208.portoalegre.pucrsnet.br>
2016-05-18 10:52:00 -0300

    Só aceita

--- wc2.c
+++ wc2.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 
 	FILE *arquivo;
 	int c;
 
-    int cl = 0;
-    int cb = 0;
-    int cp = 0;
+	int cl = 0;
+	int cb = 0;
+	int cp = 0;
 
-	arquivo = fopen("fatorial.c", "r");
+	arquivo = fopen("sla", "r");
 
 	c = getc(arquivo);
     while( c != -1 )
-    {
-        cb++;
-        if (c == '\n')
-            cl = cl + 1;
-        if (c == ' ')
-            cp = cp + 1;
+ 	{
+	cb++;
+	if (c == '\n')
+		cl = cl + 1;
+	if (c == ' ')
+		cp = cp + 1;
 
 		printf("%i %c %x\n", c, c, c);
 		c = getc(arquivo);
 	}
 
-    printf("L=%i P=%i B=%i\n", cl, cp, cb);
+	printf("L=%i P=%i B=%i\n", cl, cp, cb);
 
 
 	fclose(arquivo);
